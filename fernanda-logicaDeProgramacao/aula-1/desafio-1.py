73bd974fd3de0aaf80b5703514a54d9c1c1b81bf
ferlimatos <fernandamatos.design@gmail.com>
2025-12-03 14:02:38 -0300

    organizando o repositório

---
+++ fernanda-logicaDeProgramacao/aula-1/desafio-1.py
@@ -0,0 +1,4 @@
+# Desafio 1
+
+nome2 = input('Qual é o seu nome?\n')
+cidade = input('Em que cidade você mora?\n')
---
+++ fernanda-logicaDeProgramacao/aula-1/desafio-2.py
@@ -0,0 +1,7 @@
+# Desafio 2
+
+n1 = int(input('Digite um número:'))
+n2 = int(input('Digite outro número:'))
+print(f'A soma dos números digitados é {n1+n2}.')
+print(f'A multiplicação dos números digitados é {n1*n2}.')
+print(f'A divisão dos números digitados é {n1/n2}.')
