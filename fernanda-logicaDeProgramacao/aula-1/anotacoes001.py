2b36d575a06189d38c08b3361a7933f7742d0069
ferlimatos <fernandamatos.design@gmail.com>
2025-12-03 14:01:41 -0300

    organizando anotações

--- fernanda-logicaDeProgramacao/aula-1/anotacoes001.py
+++ fernanda-logicaDeProgramacao/aula-1/anotacoes001.py
@@ -1,5 +1,3 @@
-'''
-
 print('Hello Word')
 
 print('1+1')
@@ -7,29 +5,19 @@ print('1'+'1')
 print(1+1)
 
 # Input = Entrada
-nome1 = input('Qual o seu nome?\n')
+nome = input('Qual o seu nome?\n')
 idade = input('Qual a sua idade?\n')
 peso = input('Qual o seu peso?\n')
-print(nome1, idade, peso)
+print(nome, idade, peso)
 
 # format f'{}' para formatação de strings
-print(f'Meu nome é {nome1}, tenho {idade} anos e peso {peso}kg.')
+print(f'Meu nome é {nome}, tenho {idade} anos e peso {peso}kg.')
 
-# Desafio 1
-nome2 = input('Qual é o seu nome?\n')
-cidade = input('Em que cidade você mora?\n')
 
+'''
 # Tipos primitivos básicos
 int 
 bool
 float 
 str 
-
 '''
-
-# Desafio 2
-n1 = int(input('Digite um número:'))
-n2 = int(input('Digite outro número:'))
-print(f'A soma dos números digitados é {n1+n2}.')
-print(f'A multiplicação dos números digitados é {n1*n2}.')
-print(f'A divisão dos números digitados é {n1/n2}.')
