6ccd1ff7fc42e35a7d4c03324ff23d7920e4ba40
Fernanda Matos <101283069+ferlimatos@users.noreply.github.com>
2025-12-03 19:22:17 -0300

    Adicionando os exercícios de fixação
    Exercícios que pedi para uma IA gerar baseado nos desafios e exercícios compartilhados nas aulas para que eu possa praticar mais vezes.

---
+++ fernanda-logicaDeProgramacao/ex006.py
@@ -0,0 +1,9 @@
+# Exercício 6
+
+# Crie uma lista [1, 2, 3, 4, 5]. Use pop() para remover o último elemento e imprima a lista.
+
+cores = ["rosa","preto","verde","vermelho"]
+print(cores)
+
+cores[3] = "roxo"
+print(f"A lista atualizada é: {cores}.")
